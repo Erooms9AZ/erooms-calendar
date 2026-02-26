c10dedf96a54df16092d7c867bf5c7d8fe43fdd3
Dave <dave@erooms.org.uk>
2026-02-26 20:34:35 +0000

    Update calendar-v3.js

--- calendar-v3.js
+++ calendar-v3.js
@@ -153,12 +153,6 @@ document.addEventListener("click",e=>{
   }
 });
 
-
-calendarEl.appendChild(headerRow);
-
-  const startOfWeek = new Date(currentWeekStart);
-  const endOfRange = new Date(startOfWeek);
-  endOfRange.setDate(startOfWeek.getDate()+13);
 /* -----------------------------
    RENDER CALENDAR
 ------------------------------- */
